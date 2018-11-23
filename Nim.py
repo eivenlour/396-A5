a666493158acb13c092a04c334683593e55c75c3
Fangting Chen <fc1@ualberta.ca>
2018-11-23 14:23:16 -0700

    updated solver

--- Nim.py
+++ Nim.py
@@ -11,7 +11,7 @@ class Nim:
         print(' '.join(map(str,self.game)))
 
     def update(self, num_of_stones, pile):
-        self.game[pile] = self.game[pile] - num_of_stones
+        self.game[pile] = self.game[pile - 1] - num_of_stones
         self.print_game()
 
     def check_end_game(self):
--- player.py
+++ player.py
@@ -10,14 +10,23 @@ class Player():
         return reduce(operator.xor, state)
     #
     def get_winning_moves(self):
+          winning_moves= [] 
           total = self.xorsum(self.P)  # xor all elements of P
           if total==0:
             print(' loss')
             return
+          pile_index = 0 
+          #j is the number of stones in pile 
           for j in self.P:
-            tj = total ^ j  # xor
-            if j >= tj:
-              print(' win: take',j - tj,'from pile with',j)
+            #i is the number of stones taken out of the pile j 
+            for i in range(0, j):
+              new_state = self.P
+              new_state[pile_index] = j - 1 
+              if self.xorsum(new_state) == True:
+                winning_moves.append([i, index + 1])
+            pile_index += 1 
+          return winning_moves
+
 
 
 def main():
