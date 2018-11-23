3c8d6872e415164ca94a77979aa44e7ee23b0e7f
Fangting Chen <fc1@ualberta.ca>
2018-11-23 12:58:26 -0700

    updated Nim class

--- Nim.py
+++ Nim.py
@@ -8,8 +8,16 @@ class Nim:
         self.num_of_piles = len(list_of_stones)
 
     def print_game(self):
-        print(self.game)
+
+        print(' '.join(map(str,self.game)))
 
     def update(self, num_of_stones, pile):
         self.game[pile] = self.game[pile] - num_of_stones
+        self.print_game()
+
+    def check_end_game(self):
+    	for item in self.game:
+    		if item != 0:
+    			return False 
+    	return True 
 
